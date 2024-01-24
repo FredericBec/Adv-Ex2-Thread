74e86c5d2ebd4f43d62e9f4b42ada601eb11ac77
Frederic Bec <bec.frederic90@outlook.fr>
2024-01-24 10:26:32 +0100

    add another solution in comment

--- src/fr/fms/thread/ThreadTime.java
+++ src/fr/fms/thread/ThreadTime.java
@@ -6,34 +6,45 @@ import java.util.Date;
 
 public class ThreadTime {
 
-  public static void main(String[] args) {
-    DateFormat df = new SimpleDateFormat("HH:mm:ss");
-    Thread thread = new Thread(new MonRunnable(1000));
+	public static void main(String[] args) {
+		DateFormat df = new SimpleDateFormat("HH:mm:ss");
+		Thread thread = new Thread(new MonRunnable(1000));
 
-    System.out.println(df.format(new Date()));
+		System.out.println(df.format(new Date()));
 
-    thread.start();
-  }
+		thread.start();
+	}
 
-  private static class MonRunnable implements Runnable {
+	private static class MonRunnable implements Runnable {
 
-	DateFormat df = new SimpleDateFormat("HH:mm:ss");
-    private long delai;
+		DateFormat df = new SimpleDateFormat("HH:mm:ss");
+		private long delai;
 
-    public MonRunnable(long delai) {
-      this.delai = delai;
-    }
+		public MonRunnable(long delai) {
+			this.delai = delai;
+		}
 
-    @Override
-    public void run() {
-    	while(true) {
-	      try {
-	        Thread.sleep(delai);
-	        System.out.println(df.format(new Date()));
-	      } catch (InterruptedException e) {
-	        e.printStackTrace();
-	      }
-    	}
-    }
-  }
+		@Override
+		public void run() {
+			while (true) {
+				try {
+					/*
+					 * Calendar cal = Calendar.getInstance();
+					 * hours = cal.get( Calendar.HOUR_OF_DAY
+					 * ); if ( hours > 12 ) hours -= 12;
+					 * minutes = cal.get( Calendar.MINUTE );
+					 * seconds = cal.get( Calendar.SECOND );
+					 * 
+					 * SimpleDateFormat formatter = new SimpleDateFormat("hh:mm:ss");
+					 * Date date = cal.getTime();
+					 * timeString = formatter.format( date );
+					 */
+					Thread.sleep(delai);
+					System.out.println(df.format(new Date()));
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
